18fc45c131b80f5880a1a8e6ab6b0912b744f06d
fjcasanova <61351919+fjcasanova@users.noreply.github.com>
2023-06-23 10:30:46 +0200

    Add files via upload

---
+++ sphere.py
@@ -0,0 +1,37 @@
+from math import sqrt as racine
+
+class sphere():
+    def __init__(self, centre, rayon, couleur, alpha, reflexion = 0, diffusion = 1):
+        self.centre = centre
+        self.rayon = rayon
+        self.couleur = couleur
+        self.alpha = alpha
+        self.diffusion = diffusion
+        self.reflexion = reflexion
+    def normal(self, M):
+        radial =  M - self.centre
+        n = radial.unitaire()
+        return n
+
+    def inter(self, depart, direction):
+
+        a = 1
+        b = - 2 * direction.unitaire().scalaire(self.centre - depart)
+        c = abs(self.centre - depart)**2 - self.rayon**2
+
+        delta = b**2 - 4 * a * c
+
+        if delta == 0 and b < 0:
+            return - b / (2 * a)
+        elif delta > 0 :
+            x1 = (- b - racine(delta)) / (2 * a)
+            x2 = (- b + racine(delta)) / (2 * a)
+
+            if min(x1, x2) < 0 and max(x1, x2) >= 0 :
+                return max(x1, x2)
+            elif min(x1, x2) >= 0 :
+                return min(x1, x2)
+            else:
+                return 0
+        else:
+            return 0
---
+++ vecteur.py
@@ -0,0 +1,40 @@
+import math
+class vecteur() :
+    def __init__(self, x, y, z) :
+        (self.x, self.y, self.z) = (x, y, z)
+
+    def __add__(self, other) :
+        return vecteur(self.x + other.x , self.y + other.y , self.z + other.z)
+
+    def __sub__(self, other) :
+        return vecteur(self.x - other.x , self.y - other.y , self.z - other.z)
+
+    def __neg__(self) :
+        return vecteur(- self.x, - self.y, - self.z)
+
+    def __mul__(self, other) :
+        return vecteur(self.x * other , self.y * other , self.z * other)
+
+    def __rmul__(self, other) :
+        return vecteur(self.x * other , self.y * other , self.z * other)
+
+    def scalaire(self, other) :
+        return self.x * other.x + self.y * other.y + self.z * other.z
+
+    def __abs__(self) :
+        return math.sqrt(self.scalaire(self))
+
+    def unitaire(self) :
+        return vecteur(self.x , self.y, self.z) * (1.0 / abs(self))
+
+    def vecteur2coord(self) :
+        return (self.x, self.y, self.z)
+
+    def rgb(self) :
+        self.x = min(self.x, 255)
+        self.x = max(self.x, 0)
+        self.y = min(self.y, 255)
+        self.y = max(self.y, 0)
+        self.z = min(self.z, 255)
+        self.z = max(self.z, 0)
+        return(self.x, self.y, self.z)
